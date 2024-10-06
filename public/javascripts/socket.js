674013a243f656ad50b960fa75fc15437149f595
K-Dipen <k-dipen@aasan.wlink.com.np>
2024-10-06 12:58:34 +0545

    added a feature that was rendered

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -60,7 +60,16 @@ middleChat.innerHTML = `                    <div class="ChatImg">
 <img src="/images/profilePictures/${profilePic}" alt="Not found!">
 </div>
 <div style="display: flex; flex-direction: column;">
-<div class="username"><span>${profileName}</span></div>`;
+
+<div class="username"><span>${profileName}</span></div>
+<% if (searchedUser.isOnline == 1){ %>
+<div class="userStatus Online" id="<%= searchedUser._id %> status"><span>Online</span></div>
+<%} else{ %>
+<div class="userStatus Offline" id="<%= searchedUser._id %> status"><span>Offline</span></div>
+<% } %>
+</div>
+<div class="icons"></div>
+`;
 
 if(chats.length>0){
 console.log(chats.length);
